75386daf13ca11a246a78c414f9b0497ff954680
KazL <kazph1707@gmail.com>
2023-11-07 20:59:36 +0900

    revise

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"github.com/oklog/ulid/v2"
 	_ "github.com/oklog/ulid/v2"
 	"log"
@@ -12,16 +13,19 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"unicode/utf8"
 )
 
-type CatalogResForHTTP struct {
-	Id                  string `json:"id"`
-	Name                string `json:"name"`
-	Item_category       string `json:"item_category"`
-	Curriculum_category string `json:"curriculum_category"`
-	Detail              string `json:"Detail"`
-	URL                 string `json:"URL"`
-	Update_time         int    `json:"update_time"`
+type UserResForHTTPGet struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type User struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 type responseMessage struct {
@@ -33,241 +37,98 @@ var db *sql.DB
 
 func init() {
 	// ①-1
+	
 	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
+	mysqlPwd := os.Getenv("MYSQL_PASSWORD")
 	mysqlHost := os.Getenv("MYSQL_HOST")
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	fmt.Print(mysqlUser, mysqlPwd, mysqlDatabase)
 
 	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
-	db, err := sql.Open("mysql", connStr)
-
+    db, err := sql.Open("mysql", connStr)
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
 	// ①-3
-	if err := db.Ping(); err != nil {
+	if err := _db.Ping(); err != nil {
 		log.Fatalf("fail: _db.Ping, %v\n", err)
 	}
-
+	db = _db
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS")
 	switch r.Method {
-	//case http.MethodGet:
-	//	// ②-1 nameが空だった場合のエラー処理を施しています
-	//	name := r.URL.Query().Get("name") // To be filled
-	//	if name == " " {
-	//		log.Println("fail: name is empty")
-	//		w.WriteHeader(http.StatusBadRequest)
-	//		return
-	//	}
-	//
-	//	// ②-2 GETリクエストのクエリパラメータから条件を満たすデータを取得します。
-	//	rows, err := db.Query("SELECT id, name, age FROM user WHERE name = ?", name)
-	//	if err != nil {
-	//		log.Printf("fail: db.Query, %v\n", err)
-	//		w.WriteHeader(http.StatusInternalServerError)
-	//		return
-	//	}
-	//
-	//	// ②-3
-	//	users := make([]CatalogResForHTTP, 0)
-	//	for rows.Next() {
-	//		var u CatalogResForHTTP
-	//		if err := rows.Scan(&u.Id, &u.Name, &u.Item_category); err != nil {
-	//			log.Printf("fail: rows.Scan, %v\n", err)
-	//
-	//			if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-	//				log.Printf("fail: rows.Close(), %v\n", err)
-	//			}
-	//			w.WriteHeader(http.StatusInternalServerError)
-	//			return
-	//		}
-	//		users = append(users, u)
-	//	}
-	//
-	//	// ②-4 モジュール encoding/json を用いてレスポンス用ユーザースライスをJSONへ変換し、HTTPレスポンスボディに書き込みます。
-	//	bytes, err := json.Marshal(users)
-	//	if err != nil {
-	//		log.Printf("fail: json.Marshal, %v\n", err)
-	//		w.WriteHeader(http.StatusInternalServerError)
-	//		return
-	//	}
-	//	w.Header().Set("Content-Type", "application/json")
-	//	w.Write(bytes)
-
-	case http.MethodPost:
-		//① idを採番
-		var user CatalogResForHTTP
-		user.Id = ulid.Make().String() //GetNewULIDString()
-		//② SQLにid, name, ageをinsert, 何らかのエラーにはinternal server error(500)
-
-		json.NewDecoder(r.Body).Decode(&user)
-		id := user.Id
-		name := user.Name
-		item_category := user.Item_category
-		curriculum_category := user.Curriculum_category
-		detail := user.Detail
-		url1 := user.URL
-		update_time := user.Update_time
-
-		//nameが空、id,nameの文字列の長さが51文字以上、ageが20未満or80より上の時BadRequest400
+	case http.MethodGet:
+		// ②-1 nameが空だった場合のエラー処理を施しています
+		name := r.URL.Query().Get("name") // To be filled
 		if name == " " {
+			log.Println("fail: name is empty")
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		tx, err := db.Begin()
-
-		_, newErr := tx.Exec("INSERT INTO catalog(id,name,item_category,curriculum_category, detail, url, update_time) VALUES(?,?,?,?,?,?,?)",
-			id, name, item_category, curriculum_category, detail, url1, update_time)
-		if newErr != nil {
-			tx.Rollback()
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		fmt.Println("Insert", user)
-
-		if err := tx.Commit(); err != nil {
+		// ②-2 GETリクエストのクエリパラメータから条件を満たすデータを取得します。
+		rows, err := db.Query("SELECT id, name, age FROM user WHERE name = ?", name)
+		if err != nil {
+			log.Printf("fail: db.Query, %v\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
-		//③ insertができたものはstatus code=200と,jsonでidを返す
-		bytes, err := json.Marshal(responseMessage{
-			Id: user.Id,
-		})
+		// ②-3
+		users := make([]UserResForHTTPGet, 0)
+		for rows.Next() {
+			var u UserResForHTTPGet
+			if err := rows.Scan(&u.Id, &u.Name, &u.Age); err != nil {
+				log.Printf("fail: rows.Scan, %v\n", err)
+
+				if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
+					log.Printf("fail: rows.Close(), %v\n", err)
+				}
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			users = append(users, u)
+		}
+
+		// ②-4 モジュール encoding/json を用いてレスポンス用ユーザースライスをJSONへ変換し、HTTPレスポンスボディに書き込みます。
+		bytes, err := json.Marshal(users)
 		if err != nil {
 			log.Printf("fail: json.Marshal, %v\n", err)
-			return
-		}
-		if err == nil {
-			w.WriteHeader(http.StatusOK)
-			w.Write(bytes)
-			return
-		}
-
-	default:
-		log.Printf("fail: HTTP Method is %s\n", r.Method)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-}
-
-func handler2(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS")
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// ②-2 全件取得
-	rows, err := db.Query("SELECT id,name,item_category,curriculum_category, detail, url, update_time FROM catalog")
-	if err != nil {
-		log.Printf("fail: db.Query, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// ②-3
-	users := make([]CatalogResForHTTP, 0)
-	for rows.Next() {
-		var u CatalogResForHTTP
-		if err := rows.Scan(&u.Id, &u.Name, &u.Item_category, &u.Curriculum_category, &u.Detail, &u.URL, &u.Update_time); err != nil {
-			log.Printf("fail: rows.Scan, %v\n", err)
-
-			if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-				log.Printf("fail: rows.Close(), %v\n", err)
-			}
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		users = append(users, u)
-	}
-
-	// ②-4 モジュール encoding/json を用いてレスポンス用ユーザースライスをJSONへ変換し、HTTPレスポンスボディに書き込みます。
-	bytes, err := json.Marshal(users)
-	if err != nil {
-		log.Printf("fail: json.Marshal, %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(bytes)
-}
-
-func handler3(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS")
-	switch r.Method {
-	//case http.MethodGet:
-	//	// ②-1 nameが空だった場合のエラー処理を施しています
-	//	name := r.URL.Query().Get("name") // To be filled
-	//	if name == " " {
-	//		log.Println("fail: name is empty")
-	//		w.WriteHeader(http.StatusBadRequest)
-	//		return
-	//	}
-	//
-	//	// ②-2 GETリクエストのクエリパラメータから条件を満たすデータを取得します。
-	//	rows, err := db.Query("SELECT id, name, age FROM user WHERE name = ?", name)
-	//	if err != nil {
-	//		log.Printf("fail: db.Query, %v\n", err)
-	//		w.WriteHeader(http.StatusInternalServerError)
-	//		return
-	//	}
-	//
-	//	// ②-3
-	//	users := make([]CatalogResForHTTP, 0)
-	//	for rows.Next() {
-	//		var u CatalogResForHTTP
-	//		if err := rows.Scan(&u.Id, &u.Name, &u.Item_category); err != nil {
-	//			log.Printf("fail: rows.Scan, %v\n", err)
-	//
-	//			if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-	//				log.Printf("fail: rows.Close(), %v\n", err)
-	//			}
-	//			w.WriteHeader(http.StatusInternalServerError)
-	//			return
-	//		}
-	//		users = append(users, u)
-	//	}
-	//
-	//	// ②-4 モジュール encoding/json を用いてレスポンス用ユーザースライスをJSONへ変換し、HTTPレスポンスボディに書き込みます。
-	//	bytes, err := json.Marshal(users)
-	//	if err != nil {
-	//		log.Printf("fail: json.Marshal, %v\n", err)
-	//		w.WriteHeader(http.StatusInternalServerError)
-	//		return
-	//	}
-	//	w.Header().Set("Content-Type", "application/json")
-	//	w.Write(bytes)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(bytes)
 
 	case http.MethodPost:
 		//① idを採番
-		var user CatalogResForHTTP
+		var user User
 		user.Id = ulid.Make().String() //GetNewULIDString()
 		//② SQLにid, name, ageをinsert, 何らかのエラーにはinternal server error(500)
 
 		json.NewDecoder(r.Body).Decode(&user)
 		name := user.Name
-
+		age := user.Age
 		//nameが空、id,nameの文字列の長さが51文字以上、ageが20未満or80より上の時BadRequest400
 		if name == " " {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
+		if utf8.RuneCountInString(name) > 50 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if age < 20 || 80 < age {
+			fmt.Println("error")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 		tx, err := db.Begin()
 
-		_, newErr := tx.Exec("DELETE from catalog WHERE name=?", name)
+		_, newErr := tx.Exec("INSERT INTO user(id,name,age) VALUES(?,?,?)", user.Id, name, age)
 		if newErr != nil {
 			tx.Rollback()
 			w.WriteHeader(http.StatusInternalServerError)
@@ -304,14 +165,12 @@ func handler3(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
-	http.HandleFunc("/catalog", handler)
-	http.HandleFunc("/catalogs", handler2)
-	http.HandleFunc("/catalog_delete", handler3)
+	http.HandleFunc("/user", handler)
 
 	// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
 	closeDBWithSysCall()
 
-	// 8080番ポートでリクエストを待ち受ける
+	// 8000番ポートでリクエストを待ち受ける
 	log.Println("Listening...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
